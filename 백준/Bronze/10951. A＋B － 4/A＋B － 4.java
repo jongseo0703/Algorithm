faadf8d55ec007ddd2f1d7a9a161cb9c162e2be1
jongseo0703 <104488738+jongseo0703@users.noreply.github.com>
2025-02-03 17:19:09 +0900

    [Bronze V] Title: A+B - 4, Time: 128 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,29 @@
+import java.util.StringTokenizer;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str;
+        int a;
+        int b;
+
+            while ((str = br.readLine()) != null) {
+
+                StringTokenizer st = new StringTokenizer(str);
+                a = Integer.parseInt(st.nextToken());
+                b = Integer.parseInt(st.nextToken());
+
+                bw.append(String.valueOf(a + b));
+                bw.newLine();
+                bw.flush();
+
+
+            }
+
+            bw.close();
+        }
+    }
