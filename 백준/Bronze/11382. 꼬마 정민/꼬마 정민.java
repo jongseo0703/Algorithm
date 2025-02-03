c612f868671edf431a4b7aeec7df8f9bbb5b3f2a
jongseo0703 <104488738+jongseo0703@users.noreply.github.com>
2025-02-03 17:20:17 +0900

    [Bronze V] Title: 꼬마 정민, Time: 212 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        long a;
+        long b;
+        long c;
+        
+        Scanner in = new Scanner(System.in);
+        
+        a = in.nextLong();
+        b = in.nextLong();
+        c = in.nextLong();
+        
+        System.out.println(a+b+c);
+        
+    }
+    
+}
