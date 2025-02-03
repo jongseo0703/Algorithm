44bd8b474db5f2421694e0ba67da5d6f567e4f32
jongseo0703 <104488738+jongseo0703@users.noreply.github.com>
2025-02-03 17:18:45 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        String k = "joonas";
+        String l;
+        
+        Scanner in = new Scanner(System.in);
+        l = in.next();
+        
+        
+        System.out.println(l+"??!");
+        
+            
+            
+    }
+    
+}
