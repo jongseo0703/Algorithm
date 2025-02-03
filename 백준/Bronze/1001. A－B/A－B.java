2d3f25ca8d0ada84f2eb37d0d4d53eb75319ac81
jongseo0703 <104488738+jongseo0703@users.noreply.github.com>
2025-02-03 16:36:51 +0900

    [Bronze V] Title: A-B, Time: 208 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        int a;
+        int b;
+        
+        Scanner in = new Scanner(System.in);
+        
+        a = in.nextInt();
+        b = in.nextInt();
+        
+        System.out.println(a-b);
+        
+    }
+    
+}
