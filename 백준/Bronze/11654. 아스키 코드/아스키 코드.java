936bb8927bb27f05ec1849fdc1159cd8410650fb
jongseo0703 <104488738+jongseo0703@users.noreply.github.com>
2025-02-03 17:20:36 +0900

    [Bronze V] Title: 아스키 코드, Time: 196 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        char c = in.next().charAt(0);
+        int n = (int)c;
+        System.out.println(n);
+
+    }
+
+}
