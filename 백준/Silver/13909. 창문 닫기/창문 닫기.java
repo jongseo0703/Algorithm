a6c9a330f75c44809cb35dd2a9f2c21b33399eab
jongseo0703 <104488738+jongseo0703@users.noreply.github.com>
2025-03-07 20:45:42 +0900

    [Silver V] Title: 창문 닫기, Time: 116 ms, Memory: 15904 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        bw.write((int)(Math.sqrt(n)) + "\n");
+
+        bw.flush();
+        bw.close();
+
+    }
+
+}
