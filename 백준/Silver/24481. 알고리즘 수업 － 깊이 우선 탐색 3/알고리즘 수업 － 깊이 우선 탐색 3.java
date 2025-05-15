a0b9c4b081502d56a39055e3aadc39b8c5ab62a4
jongseo0703 <104488738+jongseo0703@users.noreply.github.com>
2025-05-15 13:22:01 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 3, Time: 940 ms, Memory: 92144 KB -BaekjoonHub

---
+++ 백준/Silver/24481. 알고리즘 수업 － 깊이 우선 탐색 3/알고리즘 수업 － 깊이 우선 탐색 3.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static ArrayList<ArrayList<Integer>> list;
+    static boolean[] isVisited;
+    static int[] depth;
+
+    static int N, M, R;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+
+        list = new ArrayList<>();
+        isVisited = new boolean[N + 1];
+        depth = new int[N + 1];
+
+        for(int i = 0; i < N + 1; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            list.get(x).add(y);
+            list.get(y).add(x);
+        }
+
+        for(int i = 1; i < N + 1; i++) {
+            Collections.sort(list.get(i));
+        }
+
+        Arrays.fill(depth,-1);
+
+        dfs(R, 0);
+
+        for(int i = 1; i < N + 1; i++) {
+            sb.append(depth[i]).append("\n");
+        }
+
+        System.out.print(sb);
+
+    }
+
+    static void dfs(int node, int d) {
+        isVisited[node] = true;
+        depth[node] = d;
+
+        for(int i : list.get(node)) {
+            if(!isVisited[i]) {
+                dfs(i, d + 1);
+            }
+        }
+    }
+
+}
