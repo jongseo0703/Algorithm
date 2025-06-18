5ae8721eb87bcc99dac77b439e3338fa4a21d41b
jongseo0703 <jongseo0703@naver.com>
2025-06-18 09:04:01 +0900

    [Silver I] Title: 돌다리, Time: 136 ms, Memory: 24156 KB -BaekjoonHub

--- 백준/Silver/12761. 돌다리/돌다리.java
+++ 백준/Silver/12761. 돌다리/돌다리.java
@@ -32,42 +32,25 @@ public class Main {
         queue.add(new int[]{start, 0});
         isVisited[start] = true;
 
-        int[] dirX = {-1, 1, -1 * A, A, -1 * B, B};
-
         while(!queue.isEmpty()) {
             int[] now = queue.poll();
             int cur = now[0];
             int cnt = now[1];
 
+            int[] dirX = {cur -1, cur + 1, cur -1 * A, cur + A, cur -1 * B, cur + B, cur * A, cur * B};
+
             if(cur == M) {
                 return cnt;
             }
 
-            for(int i = 0; i < 6; i++) {
-                int dx = cur + dirX[i];
+            for(int i = 0; i < 8; i++) {
+                int dx = dirX[i];
 
                 if(dx >= 0 && dx < 100001) {
                     if(!isVisited[dx]) {
                         isVisited[dx] = true;
                         queue.add(new int[]{dx, cnt + 1});
                     }
-                }
-            }
-
-            int dx2 = cur * A;
-            int dx3 = cur * B;
-
-            if(dx2 >= 0 && dx2 < 100001) {
-                if(!isVisited[dx2]) {
-                    isVisited[dx2] = true;
-                    queue.add(new int[]{dx2, cnt + 1});
-                }
-            }
-
-            if(dx3 >= 0 && dx3 < 100001) {
-                if(!isVisited[dx3]) {
-                    isVisited[dx3] = true;
-                    queue.add(new int[]{dx3, cnt + 1});
                 }
             }
         }
