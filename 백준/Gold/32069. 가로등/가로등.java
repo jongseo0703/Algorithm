aa9e429126f48c18e52fd4f43d5127784c5e6847
JongSeo <jongseo0703@naver.com>
2025-07-21 10:58:26 +0900

    [Gold V] Title: 가로등, Time: 1456 ms, Memory: 251208 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/32069. 가로등/가로등.java
+++ 백준/Gold/32069. 가로등/가로등.java
@@ -2,50 +2,70 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+  static long[] dirX = {-1, 1};
+
+  static Set<Long> isVisited;
+  static List<Long> distList;
+
+  static Long L;
+  static int N, K;
+
   public static void main(String[] args) throws IOException {
 
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
 
-    long L = Long.parseLong(st.nextToken());
-    int N = Integer.parseInt(st.nextToken());
-    int K = Integer.parseInt(st.nextToken());
+    StringBuilder sb = new StringBuilder();
+
+    StringTokenizer st;
 
     st = new StringTokenizer(br.readLine());
 
+    L = Long.parseLong(st.nextToken());
+    N = Integer.parseInt(st.nextToken());
+    K = Integer.parseInt(st.nextToken());
+
+    isVisited = new HashSet<>();
+    distList = new ArrayList<>();
+
     Queue<long[]> queue = new LinkedList<>();
-    Set<Long> visited = new HashSet<>();
 
-    for (int i = 0; i < N; i++) {
+    st = new StringTokenizer(br.readLine());
+
+    for(int i = 0; i < N; i++) {
       long x = Long.parseLong(st.nextToken());
-      queue.add(new long[]{x, 0});
-      visited.add(x);
+      queue.add(new long[]{x, 0L});
+      isVisited.add(x);
     }
 
-    List<Long> answer = new ArrayList<>();
-    int[] dir = {-1, 1};
+    bfs(queue);
 
-    while (!queue.isEmpty() && answer.size() < K) {
-      long[] cur = queue.poll();
-      long x = cur[0];
-      long d = cur[1];
-
-      answer.add(d);
-
-      for (int i = 0; i < 2; i++) {
-        long nx = x + dir[i];
-        if (nx >= 0 && nx <= L && !visited.contains(nx)) {
-          queue.add(new long[]{nx, d + 1});
-          visited.add(nx);
-        }
-      }
-    }
-
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < K; i++) {
-      sb.append(answer.get(i)).append("\n");
+    for(int i = 0; i < K; i++) {
+      sb.append(distList.get(i)).append("\n");
     }
 
     System.out.print(sb);
+
   }
+
+  static void bfs(Queue<long[]> queue) {
+    while(!queue.isEmpty() && distList.size() < K) {
+      long[] now = queue.poll();
+      long curX = now[0];
+      long dist = now[1];
+
+      distList.add(dist);
+
+      for(int i = 0; i < 2; i++) {
+        long nextX = curX + dirX[i];
+
+        if(nextX >= 0 && nextX <= L) {
+          if (!isVisited.contains(nextX)) {
+            queue.add(new long[]{nextX, dist + 1});
+            isVisited.add(nextX);
+          }
+        }
+      }
+    }
+  }
+
 }
