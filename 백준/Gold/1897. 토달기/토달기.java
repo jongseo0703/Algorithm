eb3a736a5a35ae2ba879669af1214a324d0b3720
JongSeo <jongseo0703@naver.com>
2025-08-20 10:14:12 +0900

    [Gold V] Title: 토달기, Time: 152 ms, Memory: 15724 KB -BaekjoonHub

---
+++ 백준/Gold/1897. 토달기/토달기.java
@@ -0,0 +1,88 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  static List<String> dict;
+  static boolean[] isVisited;
+
+  static int d;
+  static String word;
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringBuilder sb = new StringBuilder();
+
+    StringTokenizer st;
+
+    st = new StringTokenizer(br.readLine());
+
+    d = Integer.parseInt(st.nextToken());
+    word = st.nextToken();
+
+    dict = new ArrayList<>();
+    isVisited = new boolean[d];
+
+    for(int i = 0; i < d; i++) {
+      dict.add(br.readLine());
+    }
+
+    String longest = bfs();
+
+    sb.append(longest);
+
+    System.out.print(sb);
+
+  }
+
+  static String bfs() {
+    Queue<String> queue = new LinkedList<>();
+    queue.add(word);
+    String longest = word;
+
+    while(!queue.isEmpty()) {
+      String cur = queue.poll();
+
+      for(int i = 0; i < d; i++) {
+        if(!isVisited[i]) {
+          String next = dict.get(i);
+
+          if(canTransform(cur, next)) {
+            isVisited[i] = true;
+            queue.add(next);
+
+            if(longest.length() < next.length()) {
+              longest = next;
+            }
+          }
+        }
+      }
+    }
+    return longest;
+  }
+
+  static boolean canTransform(String cur, String next) {
+    if(cur.length() + 1 != next.length()) {
+      return false;
+    }
+
+    int i = 0, j = 0, diff = 0;
+    while(i < cur.length() && j < next.length()) {
+      if(cur.charAt(i) == next.charAt(j)) {
+        i++;
+        j++;
+      }
+      else {
+        diff++;
+        j++;
+        if(diff > 1) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+}
