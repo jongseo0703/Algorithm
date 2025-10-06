251bfc7295823d499e6d2d63f897332bda513027
JongSeo <jongseo0703@naver.com>
2025-10-06 19:21:11 +0900

    [Silver V] Title: 뒤집기, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String s = br.readLine();
+        
+        int zeroCount = 0;
+        int oneCount = 0;
+        
+        if (s.charAt(0) == '0') {
+            zeroCount++;
+        } else {
+            oneCount++;
+        }
+        
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) != s.charAt(i - 1)) {
+                if (s.charAt(i) == '0') {
+                    zeroCount++;
+                } else {
+                    oneCount++;
+                }
+            }
+        }
+        
+        System.out.println(Math.min(zeroCount, oneCount));
+    }
+}
