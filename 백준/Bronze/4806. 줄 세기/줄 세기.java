fff2fa9d293ae0b0c8aea909589ba596b0194378
JongSeo <jongseo0703@naver.com>
2026-01-08 18:43:11 +0900

    [Bronze III] Title: 줄 세기, Time: 164 ms, Memory: 17384 KB -BaekjoonHub

---
+++ 백준/Bronze/4806. 줄 세기/줄 세기.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        int cnt = 0;
+
+        while(true) {
+            try {
+                String str = scan.nextLine();
+                cnt++;
+            } catch (Exception e) {
+                break;
+            }
+            
+        }
+        
+        System.out.println(cnt);
+        scan.close();
+    }
+}
